1b433bf55e9357579a15db4c63241ddc4c28a788
Sean Pelser <sean.pelser97@gmail.com>
2023-08-03 12:33:24 +0100

    Create fetcher.js
    Init commit

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+
+class Fetcher {
+
+    GetJSON = async (url) => {
+        this.getUrl = url
+
+        this._getData = await fetch(this.getUrl)
+        this._getJson = await this._getData.json()
+        return this._getJson
+    }
+    
+    PostJSON = async (url, obj) => {
+        this.postUrl = url
+        this.postObj = obj
+        
+        this._postData = await fetch(this.postUrl, {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.postObj)
+        })
+    
+        this._content = await this._postData.json()
+        return this._content
+    }
+
+}
